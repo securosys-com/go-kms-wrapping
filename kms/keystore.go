a8386b33947a0bb38a31e52dc0920e1c5f62b8c8
Jonas Köhnen <jonas.koehnen@sap.com>
2025-10-07 14:47:04 +0200

    Split AssumableKeyStore out from KeyStore
    Some providers cannot support one-shot operations (such as PKCS#11).
    This makes their implementation difficult:
    
    - The provider could return a static error from AssumeKeyByX methods.
      This works, but isn't a great way to test for support for assumed
      keys. Preferably, a key store agnostic caller could conveniently check
      for support, else fall back to the GetKeyByX family of methods.
    
    - The provider could ignore the intention to assume keys and execute
      a fetch right away. This does not necessarily work because the
      AssumeKeyByX methods lack a context parameter.
    
     - The provider could implement a lazy key type that eventually performs
       multi-shot operations, but not at key assumption time. This works,
       but seems like unnecessary extra effort.
    
    By splitting the interface off, it becomes both:
    
    1. Optional to implement.
    2. Easy to check support via type assertions.
    
    Once we need this to work over gRPC, we can add a SupportsAssumption()
    method to KeyStore.
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- kms/keystore.go
+++ kms/keystore.go
@@ -12,8 +12,9 @@ const (
 	GroupAttr string = "group"
 )
 
-// KeyStore represents the keys life-cycle management interface that must be implemented by any crypto provider.
-// This is the entry point for creating  Key objects.
+// KeyStore represents the keys life-cycle management interface that must be
+// implemented by any crypto provider. This is the entry point for creating Key
+// objects.
 type KeyStore interface {
 	// Close terminates the keystore
 	Close(ctx context.Context) error
@@ -35,6 +36,14 @@ type KeyStore interface {
 	// GetKeyByAttrs searches for the key with the specified attributes.
 	GetKeyByAttrs(ctx context.Context, attrs map[string]interface{}) (Key, error)
 
+	// GetInfo returns information about the provider to the caller for
+	// end-user display purposes.
+	GetInfo() map[string]string
+}
+
+// AssumableKeyStore provides additional methods that [KeyStore] can implement
+// to support one-shot operations.
+type AssumableKeyStore interface {
 	// AssumeKeyById returns an unresolved key which can be used on subsequent
 	// requests but without fully resolving all details; this allows one-shot
 	// sign/encrypt operations without first looking up the key attributes.
@@ -49,8 +58,4 @@ type KeyStore interface {
 	// requests but without fully resolving all details; this allows one-shot
 	// sign/encrypt operations without first looking up the key attributes.
 	AssumeKeyByAttrs(attrs map[string]interface{}) (Key, error)
-
-	// GetInfo returns information about the provider to the caller for
-	// end-user display purposes.
-	GetInfo() map[string]string
 }
